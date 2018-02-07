dd79a4ea5cfed1ce9c33713a113fe8b8d77a6ee3
Mujib517 <mujib517@gmail.com>
2018-02-07 07:19:24 +0530

    Deployment ready

--- index.js
+++ index.js
@@ -8,21 +8,22 @@ var defaultRouter = require('./routes/default.router');
 var productRouter = require('./routes/product.router');
 var userRouter = require('./routes/user.router');
 var isAuthenticated = require("./utilities/middlewares");
+var config = require("./utilities/config");
 
 var app = express();
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server is running on port " + port);
 });
 
-mongoose.connect("mongodb://localhost/myproductsdb");
+mongoose.connect(config.conStr);
 
 app.use(bodyParser.json());
 
 app.use('/', defaultRouter);
 app.use('/api/users', userRouter);
 
-
-
-app.use(isAuthenticated);
+//app.use(isAuthenticated);
 app.use('/api/products', productRouter);
 
--- utilities/config.js
+++ utilities/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     password: "secret",
-    expiry: '1d'
+    expiry: '1d',
+    //conStr: "mongodb://localhost/myproductsdb",
+    conStr: "mongodb://admin:admin@ds125388.mlab.com:25388/myproductsdb"
 };
